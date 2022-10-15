4cd5d3e75bd9cff82ced0f8c7b4a2f403b50ba59
Luyanda07 <85372048+Luyanda07@users.noreply.github.com>
2022-10-15 17:53:31 +0200

    Add files via upload

---
+++ FileReader.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filereader;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author hp
+ */
+public class FileReader {
+
+    public FileReader(String thobani) {
+    }
+
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+      Scanner input = new Scanner(System.in);
+      System.out.println("Enter the path of the file: ");
+      String pathStr = input.nextLine();
+      
+      System.out.println("Enter the name of the file: ");
+      String file = input.nextLine();
+      
+      System.out.println(file);      
+      File dir = new File(pathStr);
+      String[] list = dir.list();
+      System.out.println(Arrays.toString(list));
+      
+      boolean flag = false;      
+      for (int i = 0; i < list.length; i++) {
+         if(file.equals(list[i])){
+            flag = true;
+         }
+      }        
+      if(flag){
+         System.out.println("File Found");
+      }else{
+         System.out.println("File Not Found");
+      }
+      
+      PrintWriter pw = new PrintWriter("Newfile.txt");
+          
+      BufferedReader br;
+        br = new BufferedReader(new FileReader("Thobani"));
+        
+        String line = br.readLine(); 
+                
+        while (line != null) 
+        { 
+            pw.println(line); 
+            line = br.readLine(); 
+        } 
+          
+        br = new BufferedReader(new FileReader("Luyanda")); 
+          
+        line = br.readLine();
+        
+        while(line != null) 
+        { 
+            pw.println(line); 
+            line = br.readLine(); 
+        } 
+          
+        pw.flush(); 
+                  
+        br.close(); 
+        pw.close(); 
+          
+        System.out.println("Done");
+   }
+    
+}
